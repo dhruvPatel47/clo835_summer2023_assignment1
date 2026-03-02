5162e83b4ee518c1a8f22327096fad0fe9fda917
EC2 Default User <ec2-user@ip-172-31-89-174.ec2.internal>
2026-03-02 00:56:29 +0000

    new files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,58 +1,21 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_ecr_repository" "app_repo" {
-  name = var.ecr_repo_name
-}
-
-resource "aws_ecr_repository" "db_repo" {
-  name = var.ecr_repo_name1
-}
-
-resource "aws_security_group" "ec2_sg" {
-  name        = "clo835-sg"
-  description = "Allow SSH and HTTP"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "app_ec2" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
+resource "aws_instance" "EC2-SRVR" {
+  ami                    = "ami-0f3caa1cf4417e51b" # Amazon Linux 2 us-east-1
+  instance_type          = "t2.medium"
+  subnet_id              = data.aws_subnets.default.ids[0]  # First default subnet
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = "vockey"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo dnf update -y
+    sudo dnf install git -y
+    sudo dnf install docker -y
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo usermod -aG docker ec2-user
+  EOF
 
   tags = {
-    Name = "clo835-ec2"
+    Name = "Web-Server"
   }
 }
